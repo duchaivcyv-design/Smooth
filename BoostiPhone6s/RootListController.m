f8fd3a50b7b080300017690afc7d05106383f793
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-25 05:17:37 +0700

    Update RootListController.m

--- BoostiPhone6s/RootListController.m
+++ BoostiPhone6s/RootListController.m
@@ -17,23 +17,17 @@
 - (void)respring:(id)sender {
     pid_t pid;
     
-    // 1. Thử gọi sbreload trên Rootless (/var/jb/usr/bin/sbreload)
-    const char *sbreloadRootless[] = {"sbreload", NULL};
-    if (posix_spawn(&pid, "/var/jb/usr/bin/sbreload", NULL, NULL, (char *const *)sbreloadRootless, NULL) == 0) {
-        return;
-    }
+    // Thử chạy sbreload
+    const char *args1[] = {"sbreload", NULL};
+    posix_spawn(&pid, "/var/jb/usr/bin/sbreload", NULL, NULL, (char *const *)args1, NULL);
     
-    // 2. Thử gọi killall trên Rootless (/var/jb/usr/bin/killall)
-    const char *killallArgs[] = {"killall", "-9", "SpringBoard", NULL};
-    if (posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char *const *)killallArgs, NULL) == 0) {
-        return;
-    }
-
-    // 3. Dự phòng cho Rootful (đường dẫn cũ)
-    if (posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char *const *)sbreloadRootless, NULL) == 0) {
-        return;
-    }
-    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)killallArgs, NULL);
+    // Thử chạy killall
+    const char *args2[] = {"killall", "-9", "SpringBoard", NULL};
+    posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char *const *)args2, NULL);
+    
+    // Thử chạy lệnh qua system
+    system("killall -9 SpringBoard");
+    system("/var/jb/usr/bin/sbreload");
 }
 
 @end
