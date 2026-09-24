e2fb133aff40910433128e2c91b8f00d64331cb6
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-25 04:58:57 +0700

    Update RootListController.m

--- BoostiPhone6s/RootListController.m
+++ BoostiPhone6s/RootListController.m
@@ -16,8 +16,24 @@
 
 - (void)respring:(id)sender {
     pid_t pid;
-    const char *args[] = {"sbreload", NULL};
-    posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char *const *)args, NULL);
+    
+    // 1. Thử gọi sbreload trên Rootless (/var/jb/usr/bin/sbreload)
+    const char *sbreloadRootless[] = {"sbreload", NULL};
+    if (posix_spawn(&pid, "/var/jb/usr/bin/sbreload", NULL, NULL, (char *const *)sbreloadRootless, NULL) == 0) {
+        return;
+    }
+    
+    // 2. Thử gọi killall trên Rootless (/var/jb/usr/bin/killall)
+    const char *killallArgs[] = {"killall", "-9", "SpringBoard", NULL};
+    if (posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char *const *)killallArgs, NULL) == 0) {
+        return;
+    }
+
+    // 3. Dự phòng cho Rootful (đường dẫn cũ)
+    if (posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char *const *)sbreloadRootless, NULL) == 0) {
+        return;
+    }
+    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)killallArgs, NULL);
 }
 
 @end
