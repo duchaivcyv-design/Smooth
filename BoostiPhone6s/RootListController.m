3498af4e48fdd5a761c7d34dc13e4b5cb9cc5f9b
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-24 13:15:40 +0700

    Create RootListController.m

---
+++ BoostiPhone6s/RootListController.m
@@ -0,0 +1,22 @@
+#Preferences/Preferences.h>
+
+@interface RootListController : PSListController
+@end
+
+@implementation RootListController
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    }
+
+    return _specifiers;
+}
+
+- (void)respring:(id)sender {
+    pid_t pid;
+    const char *args[] = {"sbreload", NULL};
+    posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char *const *)args, NULL);
+}
+
+@end
