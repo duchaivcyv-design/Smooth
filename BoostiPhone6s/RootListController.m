85245f5e462929c56215998cb835138f291bc848
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-25 16:14:52 +0700

    Refactor respring method and add resetSafeMode

--- BoostiPhone6s/RootListController.m
+++ BoostiPhone6s/RootListController.m
@@ -1,41 +1,79 @@
-#import <Preferences/PSListController.h>
+#import "RootListController.h"
 #import <Preferences/PSSpecifier.h>
 #import <spawn.h>
+#import <UIKit/UIKit.h>
 
-@interface RootListController : PSListController
-@end
+// Import header internal để truy cập CrashGuard/SystemBlocker reset function
+// Lưu ý: Chỉ import khi build subproject, không ảnh hưởng main library
+#ifdef BUILDING_SUBPROJECT
+#import "../Modules/CrashGuard.h"
+#import "../Modules/SystemBlocker.h"
+#endif
 
 @implementation RootListController
 
-- (NSArray *)specifiers {
+- (NSArray<PSSpecifier *> *)specifiers {
     if (!_specifiers) {
         _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
     }
     return _specifiers;
 }
 
-- (void)respring:(id)sender {
+- (void)resetSafeMode:(id)sender {
+    UIAlertController *confirm = [UIAlertController alertControllerWithTitle:@"Xác nhận thoát Safe Mode?"
+                                                                     message:@"Tất cả tính năng tối ưu sâu sẽ được bật lại.\nMáy sẽ tự động respring sau khi xác nhận."
+                                                              preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"Đồng ý" 
+                                                      style:UIAlertActionStyleDestructive 
+                                                    handler:^(UIAlertAction * _Nonnull action) {
+        #ifdef BUILDING_SUBPROJECT
+            [[CrashGuard sharedInstance] resetSafeModeManually];
+            [[SystemBlocker sharedInstance] resetSafeModeManually];
+        #endif
+        
+        // Force reload specifiers để cập nhật UI ngay lập tức
+        [self reloadSpecifiers];
+        
+        // Respring để áp dụng trạng thái mới
+        [self performRespring];
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Hủy" 
+                                                         style:UIAlertActionStyleCancel 
+                                                       handler:nil];
+    
+    [confirm addAction:yesAction];
+    [confirm addAction:cancelAction];
+    [self presentViewController:confirm animated:YES completion:nil];
+}
+
+- (void)performRespring {
     pid_t pid;
     
-    // 1. Chạy sbreload cho Rootless (/var/jb/usr/bin/sbreload)
+    // 1. sbreload Rootless (Ưu tiên cao nhất cho Dopamine/Palera1n)
     const char *args1[] = {"sbreload", NULL};
     if (posix_spawn(&pid, "/var/jb/usr/bin/sbreload", NULL, NULL, (char *const *)args1, NULL) == 0) {
         return;
     }
     
-    // 2. Chạy killall cho Rootless (/var/jb/usr/bin/killall)
+    // 2. killall SpringBoard Rootless
     const char *args2[] = {"killall", "-9", "SpringBoard", NULL};
     if (posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char *const *)args2, NULL) == 0) {
         return;
     }
 
-    // 3. Dự phòng cho Rootful (/usr/bin/sbreload)
+    // 3. Fallback Rootful (cho TrollStore/Legacy JB)
     if (posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char *const *)args1, NULL) == 0) {
         return;
     }
 
-    // 4. Dự phòng cho Rootful (/usr/bin/killall)
+    // 4. Last resort: killall Rootful
     posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args2, NULL);
+}
+
+- (void)respring:(id)sender {
+    [self performRespring];
 }
 
 @end
