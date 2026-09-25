b3fe1111769b9acb45d81a641c83358082a45b8f
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-26 03:20:36 +0700

    Update RootListController.h

--- BoostiPhone6s/RootListController.h
+++ BoostiPhone6s/RootListController.h
@@ -7,18 +7,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RootListController : PSListController
 
-/**
- * Xử lý nút Respring thủ công từ Settings.
- */
 - (void)respring:(id)sender;
-
-/**
- * Xử lý thoát Safe Mode và reset cấu hình.
- */
 - (void)resetSafeMode:(id)sender;
 
 @end
 
 NS_ASSUME_NONNULL_END
 
-#endif /* ROOTLISTCONTROLLER_H */
+#endif
