3faff5cc39a373a8d5c96ef870f88fae16d6d97a
duchaivcyv-design <duchaivcyv@icloud.com>
2026-09-25 21:41:50 +0700

    Update CacheCleaner.h

--- Modules/CacheCleaner.h
+++ Modules/CacheCleaner.h
@@ -5,16 +5,11 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/**
- * Module dọn dẹp cache hệ thống và RAM.
- * Hỗ trợ cả standard purge và deep Mach-level purge.
- * An toàn tuyệt đối trên iOS 14-26 Rootless.
- */
 @interface CacheCleaner : NSObject
 
 + (unsigned long long)cleanDirectoryAtPath:(NSString *)path;
 + (unsigned long long)getDirectorySize:(NSString *)path;
-+ (void)cleanupTempFiles;
++ (unsigned long long)cleanupTempFiles;
 + (void)forceMemoryPurge;
 + (void)forceDeepMemoryPurge;
 + (void)clearURLCache;
